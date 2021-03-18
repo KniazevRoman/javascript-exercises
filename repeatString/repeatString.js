ae8aa6cd7e77ff09f8e5461648cd40cce1692539
Roman Kniazev <kniazev.re@gmail.com>
2021-03-18 21:09:43 +0300

    repeat String

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, repeats) {
+    if (repeats < 0) {
+        return 'ERROR';
+    } 
 
+    let result = '';
+
+    for (let i = 0; i < repeats; i++) {
+        result += string;
+    }
+
+    return result;
 }
 
 module.exports = repeatString
