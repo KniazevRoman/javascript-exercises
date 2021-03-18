94aaf7e1cb61cad91a21acc1df2ed35dff62323b
Roman Kniazev <kniazev.re@gmail.com>
2021-03-18 22:12:30 +0300

    temp conversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,21 @@
-const ftoc = function() {
+const ftoc = function(deg) {
+  let result = (deg - 32) * 5 / 9;
 
+  if (Number.isInteger(result)) {
+    return result;
+  } else {
+    return +result.toFixed(1);
+  }
 }
 
-const ctof = function() {
+const ctof = function(deg) {
+  let result = (deg * 9 / 5) + 32;
 
+  if (Number.isInteger(result)) {
+    return result;
+  } else {
+    return +result.toFixed(1);
+  }
 }
 
 module.exports = {
