f2317a960c11aa1b1491921147f993fb7a657af3
Roman Kniazev <kniazev.re@gmail.com>
2021-03-18 21:48:27 +0300

    sum all

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let result = 0;
 
+    if(a < 0 || b < 0 ) {
+        return "ERROR";
+    }
+
+    if (!(typeof a === 'number') || !(typeof b === 'number')) {
+        return "ERROR";
+    }
+
+    if (a < b) {
+        for (let i = a; i <= b; i++) {
+            result += i;
+        }
+    }
+
+    if (a > b) {
+        for (let i = b; i <= a; i++) {
+            result += i;
+        }
+    }
+
+    return result;
 }
 
 module.exports = sumAll
