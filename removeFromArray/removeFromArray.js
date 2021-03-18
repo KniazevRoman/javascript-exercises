adda6e77872591fccb738c3e4414f6cb9d562b95
Roman Kniazev <kniazev.re@gmail.com>
2021-03-18 21:36:02 +0300

    remove from array

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...rest) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let item of rest) {
+            if (arr[i] === item) {
+                arr.splice(i,1);
+                i--;
+            }
+        }
+    }
 
+    return arr;
 }
 
 module.exports = removeFromArray
