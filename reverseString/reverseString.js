81e04d842bc85ead4c157a15f1713572cc426ce9
Roman Kniazev <kniazev.re@gmail.com>
2021-03-18 21:28:26 +0300

    index.html and reverse String task

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let arr = string.split("");
+    let reverseArr = [];
 
+    for (let i = (arr.length - 1); i >= 0; i-- ) {
+        reverseArr.push(arr[i]);
+    }
+
+    return reverseArr.join("");
 }
+
 
 module.exports = reverseString
