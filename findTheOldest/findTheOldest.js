f0cc275b334e6560e565eefc750d712adcfe816c
Roman Kniazev <kniazev.re@gmail.com>
2021-06-12 11:16:09 +0300

    advanced features

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-let findTheOldest = function() {
+let findTheOldest = function(people) {
+    people.sort((a, b) => {
+        let currentYear = new Date().getFullYear();
+        return  ((b.yearOfDeath || currentYear) - b.yearOfBirth) - ((a.yearOfDeath || currentYear) - a.yearOfBirth);
+    });
 
+    return people[0];
 }
 
 module.exports = findTheOldest
